23c54a392cb274645540d0a38a8e8ace2df985f4
james <jameslindsay2220@gmail.com>
2023-11-04 22:29:54 +0000

    all json completed and endpoints made

---
+++ routes/videos.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+const router = express.Router();
+
+router.get("/", (_req, res) => {
+    const videoDetails = fs.readFileSync("./data/videos.json");
+    const parsedVideoDetails = JSON.parse(videoDetails);
+    res.json(parsedVideoDetails);
+    console.log(parsedVideoDetails)
+});
+
+router.get("/:id", (req, res) => {
+    const videoId = req.params.id;
+    const videoDetails = fs.readFileSync("./data/videos.json");
+    const parsedVideoDetails = JSON.parse(videoDetails);
+    
+    // Find the video with the specified ID
+    const selectedVideo = parsedVideoDetails.find(video => video.id === videoId);
+
+    if (selectedVideo) {
+        res.json(selectedVideo);
+    } else {
+        res.status(404).json({ message: "Video not found" });
+    }
+});
+
+
+
+router.post("/", (req, res) => {
+    const { title, description, likes, views, timestamp, channel, image} = req.body;
+  
+    const newVideo = {
+      id: uuid(),
+      title: title,
+      description: description,
+      channel: channel,
+      likes: likes,
+      views: views,
+      timestamp: timestamp,
+      image: image,
+    };
+    const videosJson = fs.readFileSync("./data/videos.json");
+    const parsedVideos = JSON.parse(videosJson);
+    parsedVideos.push(newVideo);
+  
+    const updatedVideoJson = JSON.stringify(parsedVideos);
+    fs.writeFileSync("./data/videos.json", updatedVideoJson);
+  
+
+    res.status(201).json(newVideo);
+  });
+module.exports = router
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+
+app.use(express.static('./public'));
+
+const PORT = process.env.PORT
+
+app.use(express.json());
+
+app.use(cors({ origin: process.env.FRONTEND_URL }));
+
+const videoRoutes = require("./routes/videos");
+app.use("/video-details", videoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
